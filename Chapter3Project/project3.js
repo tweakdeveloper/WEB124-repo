94a9d26309ff698416f0d6772bd6b40642164a25
Nicola Clark <nicola@slottedspoon.dev>
2025-03-30 23:42:27 -0500

    [Project3] skeleton HTML + completed script

---
+++ Chapter3Project/project3.js
@@ -0,0 +1,29 @@
+// Nicola Clark - 30MAR25
+
+// Q1
+const myName = 'Nicola Clark';
+console.log(myName);
+
+// Q2
+const myDesiredAnnualSalary = 60_000;
+console.log(myDesiredAnnualSalary);
+
+// Q3
+const amIAVeteran = false;
+console.log(amIAVeteran);
+
+// Q4
+const threeOfMyFriends = ['Jay', 'Alssya', 'Tonisha'];
+console.log(threeOfMyFriends);
+
+// Q5
+const myThreeFriendsDesiredSalaries = [84_000, 72_000, 60_000];
+console.log(myThreeFriendsDesiredSalaries);
+
+// Q5
+const myFourthFriend = {
+  firstName: 'Colton',
+  lastName: 'H',
+  desiredSalary: 84_000,
+};
+console.log(myFourthFriend);
